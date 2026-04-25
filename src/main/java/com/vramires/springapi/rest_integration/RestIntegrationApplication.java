f6b66546487d2f3235dcf428e2717d478c1a4f82
veronica.ramires <veronica_ramires@live.com>
2026-04-25 11:28:06 -0400

    first commit

---
+++ src/main/java/com/vramires/springapi/rest_integration/RestIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.vramires.springapi.rest_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vramires/springapi/rest_integration/RestIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vramires.springapi.rest_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
